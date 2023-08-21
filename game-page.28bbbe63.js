424ddd68e4901965893124f7f7cd8ac1e24f6526
marinka03 <97391927+marinka03@users.noreply.github.com>
2023-08-21 21:06:56 +0000

    Deploying to gh-pages from @ marinka03/Inquisition@c49a5bbe75e94a3f72855b2ce07fe6c40dbcc995 🚀

--- game-page.28bbbe63.js
+++ game-page.28bbbe63.js
@@ -1,2 +1,2 @@
 const e=document.getElementById("witch"),t=document.getElementById("fire");document.getElementById("arrow");console.log(t),console.log(e),document.addEventListener("keydown",(function(t){!function(){"jump"!=e.classList&&e.classList.add("jump");setTimeout((function(){e.classList.remove("jump")}),600)}()}));
-//# sourceMappingURL=index.910bcd0d.js.map
+//# sourceMappingURL=game-page.28bbbe63.js.map
--- game-page.c4133ae4.js
+++ game-page.c4133ae4.js
@@ -1,2 +1,2 @@
 !function(){var e=document.getElementById("witch"),t=document.getElementById("fire");document.getElementById("arrow");console.log(t),console.log(e),document.addEventListener("keydown",(function(t){!function(){"jump"!=e.classList&&e.classList.add("jump");setTimeout((function(){e.classList.remove("jump")}),600)}()}))}();
-//# sourceMappingURL=index.bb9f052e.js.map
+//# sourceMappingURL=game-page.c4133ae4.js.map
